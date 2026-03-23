23d91faa9fd781710c3c644ec274542ed448f5af
Rowusuduah <Rowusuduah@users.noreply.github.com>
2026-03-23 11:53:57 -0400

    fix: bump service worker cache to v2 to force login gate reload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // TODO: bump CACHE_NAME (e.g. myapp-v2) every time you deploy changes to CSS/JS/HTML
-const CACHE_NAME = 'sdt-v1';
+const CACHE_NAME = 'sdt-v2';
 const APP_SHELL  = [
   './index.html',
   './css/styles.css',
